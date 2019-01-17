65211841686eea3f4429867244dfc203f262ab1b
Liang Yi <ly@edge.ac>
2019-01-17 18:31:27 +0800

    完善SplitTransformer

--- src/Transformer/SplitTransformer.php
+++ src/Transformer/SplitTransformer.php
@@ -6,6 +6,10 @@ use Symfu\SimpleValidation\TransformerInterface;
 class SplitTransformer implements TransformerInterface {
     public function transform($direction, $value, $args = null, $fieldName = null, $formValues = []) {
         if($direction === TransformerInterface::TRANSFORM_IN) {
+            if(strlen($value) === 0) {
+                return [];
+            }
+
             $quoted = preg_quote($args);
             $pattern = "/{$quoted}/";
             return preg_split($pattern, $value);
