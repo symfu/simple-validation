677fb9b5c51eed8bd2bb21036de314caa929a05d
Liang Yi <ly@edge.ac>
2019-01-19 18:14:05 +0800

    Refactored EnumValidator to support array

--- src/Validator/EnumValidator.php
+++ src/Validator/EnumValidator.php
@@ -4,19 +4,22 @@ namespace Symfu\SimpleValidation\Validator;
 use Symfu\SimpleValidation\ValidatorInterface;
 
 class EnumValidator implements ValidatorInterface {
+
     const message = 'validation.errors.enum';
 
     public function validate($value, $argument = null, $fieldName = null, $formValues = []) {
-        if(strlen($value) === 0) {
+        if ((is_scalar($value) && strlen($value) === 0) || (is_array($value) && !$value)) {
             return [true, null];
         }
 
         $enums = $this->parse($argument);
 
-        if (in_array($value, $enums)) {
-            return [true, null];
+        if (is_array($value)) {
+            // 如果不是所有值都在枚举范围内，返回 false，否则返回 true
+            return array_diff($value, $enums) ? [false, self::message] : [true, null];
         } else {
-            return [false, self::message];
+            // 如果值不在枚举范围内，返回 false，否则返回 true
+            return !in_array($value, $enums)  ? [false, self::message] : [true, null];
         }
     }
 
@@ -28,7 +31,7 @@ class EnumValidator implements ValidatorInterface {
             $validEnums = array_values($enumValues);
         }
 
-        if(!$validEnums) {
+        if (!$validEnums) {
             throw new \InvalidArgumentException("Invalid arguments for EnumValidator: " . json_encode($enumValues));
         }
 
